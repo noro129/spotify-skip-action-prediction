d86477ea6df73c534ef792f50bac40bb3355cf0c
noro129 <109439099+noro129@users.noreply.github.com>
2022-09-30 16:43:23 +0100

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn SpotifySkip.wsgi --log-file -
---
+++ base/views.py
@@ -0,0 +1,37 @@
+import os
+import pickle
+from django.shortcuts import render
+from SpotifySkip import settings
+
+def predict(list):
+    path=os.path.join(settings.MODEL_ROOT, 'my_model.pkl')
+    model = pickle.load(open(path, 'rb'))
+    result=model.predict([list])
+    return result[0]
+
+def index(request):
+    if request.method=="POST" :
+        X=[]
+        X+=[request.POST['session_length']]
+        X+=[request.POST['no_pause_before_play']]
+        X+=[request.POST['hist_user_behavior_n_seekfwd']]
+        X+=[request.POST['hour_of_day']]
+        X+=[request.POST['context_type']]
+        X+=[request.POST['hist_user_behavior_reason_start']]
+        X+=[request.POST['context_switch']]
+        X+=[request.POST['hist_user_behavior_reason_end']]
+        X+=[request.POST['session_position']]
+        X+=[request.POST['weekday']]
+        X+=[request.POST['premium']]
+        X+=[request.POST['hist_user_behavior_n_seekback']]
+        try :
+            X=[float(i) for i in X]
+        except:
+            return render(request,'index.html',{'prediction':"error occured"})
+        prediction=predict(X)
+        if prediction==1:
+            prediction='music will be skipped'
+        else :
+            prediction='music will not be skipped'
+        return render(request,'index.html',{'prediction':prediction})
+    return render(request,'index.html')
